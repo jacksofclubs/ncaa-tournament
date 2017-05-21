7ddc5a2a1f37b90e0099ba16ce63b6edd9590cea
jacksofclubs <kevinjackswi@gmail.com>
2017-05-20 22:59:55 -0500

    Add Bracket model and migration

---
+++ ncaa/database/migrations/2017_05_21_032915_create_brackets_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBracketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('brackets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('team_id');
+            $table->integer('user_id');
+            $table->string('region');
+            $table->integer('seed');
+            $table->integer('points');
+            $table->boolean('active');
+            $table->integer('furthest_round');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('brackets');
+    }
+}
