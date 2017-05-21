53c384d86232e79235f176c770f7bd7898f3d0a4
jacksofclubs <kevinjackswi@gmail.com>
2017-05-20 22:59:33 -0500

    Add Team model and migration

---
+++ ncaa/database/migrations/2017_05_21_030111_create_teams_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('school_name');
+            $table->string('short_name')->nullable();
+            $table->string('conference')->nullable();
+            $table->string('mascot')->nullable();
+            $table->string('state')->nullable();
+            $table->string('image_file')->nullable();
+            $table->string('website')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teams');
+    }
+}
