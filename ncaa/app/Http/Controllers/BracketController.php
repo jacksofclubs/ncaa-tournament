062341ba5516c147b9bd7cc0ee9fb3833772fa50
jacksofclubs <kevinjackswi@gmail.com>
2017-05-21 16:58:21 -0500

    Added Bracket controller using scaffolded code

---
+++ ncaa/app/Http/Controllers/BracketController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Bracket;
+use Illuminate\Http\Request;
+
+class BracketController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Bracket  $bracket
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Bracket $bracket)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Bracket  $bracket
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Bracket $bracket)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Bracket  $bracket
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Bracket $bracket)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Bracket  $bracket
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Bracket $bracket)
+    {
+        //
+    }
+}
