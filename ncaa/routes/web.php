676b1186f005f8f9b9ab1fd044addf95965562c2
jacksofclubs <kevinjackswi@gmail.com>
2017-05-27 04:37:29 -0500

    Add comments to web routes

--- ncaa/routes/web.php
+++ ncaa/routes/web.php
@@ -11,10 +11,14 @@
 |
 */
 
+// Welcome page with links to home, laravel, & laracasts
+// Also log in to application
 Route::get('/', function () {
     return view('welcome');
 });
 
 Auth::routes();
 
+// Home page of the application
+// Shows that you are logged in
 Route::get('/home', 'HomeController@index')->name('home');
