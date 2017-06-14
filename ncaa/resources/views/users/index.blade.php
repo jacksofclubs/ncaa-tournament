be08d07b0c208cd72222349e1cdb54e5dc4082bc
jacksofclubs <kevinjackswi@gmail.com>
2017-06-13 21:30:45 -0500

    Add alert message to Users index page

--- ncaa/resources/views/users/index.blade.php
+++ ncaa/resources/views/users/index.blade.php
@@ -1,6 +1,13 @@
 @extends ('layouts.app')
 
 @section ('content')
+    {{-- Display flash message if needed --}}
+    @if (session('flash_message'))
+        <div class="alert alert-success">
+            {{ session('flash_message') }}
+        </div>
+    @endif
+
     <div class="row">
         <div class="col-md-12">
             <h1>Users</h1>
@@ -14,6 +21,11 @@
                     <li><a href="{{ url('/users/' . $user->id) }}">{{ $user->first_name }} {{ $user->last_name }}</a></li>
                 @endforeach
             </ul>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <a href="{{ url('/users/create') }}" class="btn btn-primary">Add User</a>
         </div>
     </div>
 @endsection
