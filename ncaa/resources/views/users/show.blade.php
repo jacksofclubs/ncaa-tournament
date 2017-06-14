94e264f50a2e8ed98956db4a3d9d96a952adc5d6
jacksofclubs <kevinjackswi@gmail.com>
2017-06-13 21:29:50 -0500

    Add buttons to Users show page

--- ncaa/resources/views/users/show.blade.php
+++ ncaa/resources/views/users/show.blade.php
@@ -15,5 +15,18 @@
             <label>Email Address: {{ $user->email }}</label>
         </div>
     </div>
+    <div class="row">
+        <div class="col-md-8">
+            <a href="{{ url()->previous() }}" class="btn btn-default">Back</a>
+            {{-- Can also do back button explicitly, like the line below --}}
+            {{-- <a href="{{ url('/users/') }}" class="btn btn-default">Back</a> --}}
+            <a href="{{ url('/users/' . $user->id . '/edit') }}" class="btn btn-primary">Edit User</a>
+            <form method="post" action="/users/{{ $user->id }}" style="display: inline">
+                {{ csrf_field() }}
+                {{ method_field('DELETE') }}
+                <button type="submit" class="btn btn-danger">Delete User</button>
+            </form>
+        </div>
+    </div>
 
 @endsection
