0e224e6a4ca28654954c69eebf63160f1061779a
jacksofclubs <kevinjackswi@gmail.com>
2017-06-14 22:17:38 -0500

    Added validation for Users edit page

--- ncaa/app/Http/Controllers/UserController.php
+++ ncaa/app/Http/Controllers/UserController.php
@@ -86,7 +86,19 @@ class UserController extends Controller
      */
     public function update(Request $request, User $user)
     {
-        //
+        $this->validate(request(), [
+            'first_name' => 'required|string|max:255',
+            'last_name'  => 'required|string|max:255',
+            'username'   => 'required|string|max:255',
+            'email'      => 'required|string|email|max:255|unique:users,email,'.$user->id
+        ]);
+
+        $user->first_name = $request->first_name;
+        $user->last_name  = $request->last_name;
+        $user->username   = $request->username;
+        $user->email      = $request->email;
+
+        $user->save();
 
         Session::flash('flash_message', 'User ' . $user->first_name . ' updated successfully!');
 
