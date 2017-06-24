a58e085b21fcb4bd3b3ab2925a9940cf8599b12f
Kevin Jacks <kevinjackswi@gmail.com>
2017-06-24 07:52:54 -0500

    Create new team object and save into database

--- ncaa/app/Http/Controllers/TeamController.php
+++ ncaa/app/Http/Controllers/TeamController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Team;
 use Illuminate\Http\Request;
+use Session;
 
 class TeamController extends Controller
 {
@@ -37,7 +38,20 @@ class TeamController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate(request(), [
+            'school_name' => 'required|string|max:255',
+            'short_name'  => 'required|string|max:8',
+            'conference'   => 'required|string|max:255'
+        ]);
+
+        Team::create(request([
+                'school_name', 'short_name', 'conference'
+            ])
+        );
+
+        Session::flash('flash_message', 'Team ' . $request->school_name . ' created successfully!');
+
+        return redirect('/teams');
     }
 
     /**
