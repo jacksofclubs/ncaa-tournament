95eeba806eb17209ea1dac65fcbdbff597dd0e50
Kevin Jacks <kevinjackswi@gmail.com>
2017-07-01 10:36:51 -0500

    Add button to create bracket

--- ncaa/resources/views/home.blade.php
+++ ncaa/resources/views/home.blade.php
@@ -12,6 +12,10 @@
         <div class="col-md-8 col-md-offset-2">
             <h2>NCAA Basketball Bracket Tournament Challenge</h2>
             <p>NCAA basketball tournament web application. Users draft teams and compete against each other. Input as many users and teams as you want. Select which eight users will be competing against each other. Participants choose teams in the draft - these will be their teams for the duration of the tournament. As the tournament progresses, participants will be awarded points when their teams win. The one who has the most points at the end of the tournament will be the winner.</p>
+
+            <div class="form-group" style="margin-top: 75px; text-align: center;">
+                <a href="{{ url('#') }}" class="btn-lg btn-primary">Create a Bracket!</a>
+            </div>
         </div>
     </div>
 @endsection
