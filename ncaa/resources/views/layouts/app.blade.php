be17ce0fbc9f605c877fd9ec10109af82148c010
Kevin Jacks <kevinjackswi@gmail.com>
2017-07-02 15:58:47 -0500

    Added font-awesome to project

--- ncaa/resources/views/layouts/app.blade.php
+++ ncaa/resources/views/layouts/app.blade.php
@@ -14,6 +14,8 @@
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <!-- Laravel Welcome Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <!-- Font-Awesome -->
+        <link href="{{ asset('font-awesome-4.7.0/css/font-awesome.min.css') }}" rel="stylesheet">
     </head>
     <body>
         <div id="app">
