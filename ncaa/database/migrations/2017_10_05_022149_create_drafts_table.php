c7d44a0c8c263171fb5339e80615ada1dd1d74a3
jacksofclubs <kevinjackswi@gmail.com>
2017-10-04 21:27:24 -0500

    Add drafts table migration

---
+++ ncaa/database/migrations/2017_10_05_022149_create_drafts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDraftsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('drafts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('short_description')->nullable();
+            $table->string('long_description')->nullable();
+            $table->integer('round')->default(0);
+            $table->integer('seconds_per_round')->default(180);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('drafts');
+    }
+}
