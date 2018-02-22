3973054f3aac61c1b7c70c1c195992f2eea41c18
jacksofclubs <kevinjackswi@gmail.com>
2018-02-21 23:23:01 -0600

    Notes on saving a new bracket

--- ncaa/app/Http/Controllers/BracketController.php
+++ ncaa/app/Http/Controllers/BracketController.php
@@ -36,7 +36,23 @@ class BracketController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // save a new instance of Bracket
+        // use short and long description from draft
+        // active and round automatically set to 0
+
+        // get the id of the newly created bracket
+        // foreach team in the draft(64 of them)
+            // save a new instance of bracket_mapping
+            // bracket_id is the id of newly created bracket
+            // user_id and team_id get from $request
+            // region and seed get from draft_teams
+            // points and wins set to 0
+            // active set to 1
+            // save the bracket_mapping instance
+
+        // delete the draft from draft
+        // could also delete everything that matches from draft_* tables, but may need some of that or save it elsewhere
+
     }
 
     /**
