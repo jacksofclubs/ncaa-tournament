7db0e0e032e310685c7e7067ef9deb80f6caccba
jacksofclubs <kevinjackswi@gmail.com>
2018-02-21 22:50:19 -0600

    Loop through each round displaying user team selections

--- ncaa/resources/views/drafts/runDraft.blade.php
+++ ncaa/resources/views/drafts/runDraft.blade.php
@@ -8,29 +8,40 @@
         </div>
     </div>
 
+    <?php 
+        $rounds = [
+            'First', 'Second', 'Third', 'Fourth',
+            'Fifth', 'Sixth', 'Seventh', 'Final'
+        ];
+    ?>
+
     <form method="POST" action="/brackets">
 
         {{ csrf_field() }}
 
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <ul>
-                    @foreach ($selectionOrder as $i => $userId)
-                        <div class="form-group">
-                            <label class="control-label col-md-2 col-md-offset-2" >User {{ $userId }}:</label>
-                            <div class="col-md-5">
-                                <select class="form-control" id="sel1" name="usersTeams[user-{{ $userId }}-pick{{ $i }}]">
-                                    <option value="" selected disabled></option>
-                                    @foreach ($teams as $team)
-                                        <option value="{{ $team->id }}">{{ $team->school_name }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                        </div>
-                    @endforeach
-                </ul>
+        @foreach ($rounds as $round)
+
+            <div class="row" style="text-align: left">
+                <div class="col-md-6 col-md-offset-2">
+                    <h3>{{ $round }} Round</h3>
+                </div>
             </div>
-        </div>
+
+            @foreach ($selectionOrder as $i => $userId)
+                <div class="form-group">
+                    <label class="control-label col-md-2 col-md-offset-2" >User {{ $userId }}:</label>
+                    <div class="col-md-5">
+                        <select class="form-control" id="sel1" name="usersTeams[user-{{ $userId }}-pick{{ $i }}]">
+                            <option value="" selected disabled></option>
+                            @foreach ($teams as $team)
+                                <option value="{{ $team->id }}">{{ $team->school_name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+            @endforeach <!-- Users -->
+
+        @endforeach <!-- Rounds -->
 
         <div class="row" style="margin-top: 30px; text-align: right;">
             <div class="form-group col-md-6 col-md-offset-3 ">
