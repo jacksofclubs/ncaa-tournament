2427e6755ed7f998c0a1a4279b13b922beacd1c9
jacksofclubs <kevinjackswi@gmail.com>
2018-02-21 22:22:47 -0600

    Add WIP runDraft page

---
+++ ncaa/resources/views/drafts/runDraft.blade.php
@@ -0,0 +1,47 @@
+@extends ('layouts.app')
+
+@section ('content')
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <h1>{{ $draft->id }} - {{ $draft->short_description }}</h1>
+            <h2>Select Each User's Teams</h2>
+        </div>
+    </div>
+
+    <form method="POST" action="/brackets">
+
+        {{ csrf_field() }}
+
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <ul>
+                    @foreach ($selectionOrder as $i => $userId)
+                        <div class="form-group">
+                            <label class="control-label col-md-2 col-md-offset-2" >{{ $userId }}</label>
+<!--                             <div class="col-md-5">
+                                <select class="form-control" id="sel1" name="ranks[{{ $i+1 }}]">
+                                    <option value="" selected disabled></option>
+                                    @foreach ($users as $user)
+                                        <option value="{{ $user->id }}">{{ $user->first_name }} {{ $user->last_name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+ -->                        </div>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+
+        <div class="row" style="margin-top: 30px; text-align: right;">
+            <div class="form-group col-md-6 col-md-offset-3 ">
+                <a href="{{ url('/drafts') }}" class="btn btn-default">Cancel</a>
+                <button type="submit" class="btn btn-primary">
+                    Save Bracket
+                    &nbsp;
+                    <i class="fa fa-angle-double-right" aria-hidden="true"></i>
+                </button>
+            </div>
+        </div>
+
+    </form>
+@endsection
