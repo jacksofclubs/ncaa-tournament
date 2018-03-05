39f72b440a7f92eb1e5a3e1f0acdf6be13c1ed78
Kevin Jacks <kevinjackswi@gmail.com>
2018-03-04 21:36:00 -0600

    Create bracket when save draft form

--- ncaa/app/Http/Controllers/BracketController.php
+++ ncaa/app/Http/Controllers/BracketController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use DB;
 use App\Bracket;
+use App\Draft;
+use App\BracketMapping;
 use Illuminate\Http\Request;
 
 class BracketController extends Controller
@@ -36,19 +39,30 @@ class BracketController extends Controller
      */
     public function store(Request $request)
     {
-        // save a new instance of Bracket
-        // use short and long description from draft
-        // active and round automatically set to 0
+        $draft = Draft::find($request->draftId);
 
-        // get the id of the newly created bracket
-        // foreach team in the draft(64 of them)
+        $bracket = Bracket::create([
+            'short_description' => $draft->short_description,
+            'long_description'  => $draft->long_description
+        ]);
+
+        foreach ($request->usersteams as $userId => $teamArray) {
+            foreach ($teamArray as $teamId) {
+                $draftTeam = DB::table('draft_teams')->where('draft_id', $draft->id)->where('team_id', $teamId)->first();
+
+                BracketMapping::create([
+                    'bracket_id' => $bracket->id,
+                    'user_id'    => $userId,
+                    'team_id'    => $teamId,
+                    'region'     => $draftTeam->region,
+                    'seed'       => $draftTeam->seed,
+                ]);
+            }
+
+            // points and wins set to 0 by default in sql - set this in migration
+            // active set to 1 by default in sql - set this in migration
             // save a new instance of bracket_mapping
-            // bracket_id is the id of newly created bracket
-            // user_id and team_id get from $request
-            // region and seed get from draft_teams
-            // points and wins set to 0
-            // active set to 1
-            // save the bracket_mapping instance
+        }
 
         // delete the draft from draft
         // could also delete everything that matches from draft_* tables, but may need some of that or save it elsewhere
